b8929ff62a2f7283303c314d6c5c03a409a6b0c2
Janghyeon Lee <janghyeon300@gmail.com>
2020-12-29 18:45:45 +0900

    초기 세팅 보강 - gitignore 수정 - ThemeProvider 추가

--- App.js
+++ App.js
@@ -4,15 +4,19 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import rootReducers from "./src/redux/reducers";
 import KiwiNavigator from "./src/navigations/KiwiNavigator";
+import { ThemeProvider } from "styled-components";
+import theme from "./src/styles/theme";
 
 const store = createStore(rootReducers);
 
 export default function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <KiwiNavigator />
-      </NavigationContainer>
+      <ThemeProvider theme={theme}>
+        <NavigationContainer>
+          <KiwiNavigator />
+        </NavigationContainer>
+      </ThemeProvider>
     </Provider>
   );
 }
