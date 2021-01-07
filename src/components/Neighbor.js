4fb2755e1f2d5b31ace0be67c7b0414688c1ac7d
Janghyeon Lee <janghyeon300@gmail.com>
2021-01-07 21:40:28 +0900

    Conflict: 충돌 해결

--- src/components/Neighbor.js
+++ src/components/Neighbor.js
@@ -1,6 +1,25 @@
 import React from "react";
 import { StyleSheet, Text, View, Button, Dimensions } from "react-native";
+import styled from "styled-components/native";
+import { flexRowMarginXView } from "../styles/mixin";
 
-const Neighbor = () => <View style={{ backgroundColor: "#111", flex: 1 }} />;
+const Neighbor = () => (
+  <Container>
+    <InnerContainer>
+      <Text>Neighbor</Text>
+    </InnerContainer>
+  </Container>
+);
 
 export default Neighbor;
+
+const Container = styled.View`
+  flex: 1;
+  background-color: white;
+`;
+
+const InnerContainer = styled.View`
+  ${flexRowMarginXView}
+  background-color: white;
+  border-bottom-color: white;
+`;
--- src/components/TownInfo.js
+++ src/components/TownInfo.js
@@ -1,6 +1,25 @@
 import React from "react";
 import { StyleSheet, Text, View, Button, Dimensions } from "react-native";
+import styled from "styled-components/native";
+import { flexRowMarginXView } from "../styles/mixin";
 
-const TownInfo = () => <View style={{ backgroundColor: "#333", flex: 1 }} />;
+const TownInfo = () => (
+  <Container>
+    <InnerContainer>
+      <Text>TownInfo</Text>
+    </InnerContainer>
+  </Container>
+);
 
 export default TownInfo;
+
+const Container = styled.View`
+  flex: 1;
+  background-color: white;
+`;
+
+const InnerContainer = styled.View`
+  ${flexRowMarginXView}
+  background-color: white;
+  border-bottom-color: white;
+`;
